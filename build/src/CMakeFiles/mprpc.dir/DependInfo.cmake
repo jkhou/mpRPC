baa6957d6bdd3f5619de75d9ac7b07c68e2359a2
jkhou <cquhjk@126.com>
2022-04-16 20:52:53 +0800

    add build file

---
+++ build/src/CMakeFiles/mprpc.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hjk/Desktop/mpRPC/src/logger.cc" "/home/hjk/Desktop/mpRPC/build/src/CMakeFiles/mprpc.dir/logger.cc.o"
+  "/home/hjk/Desktop/mpRPC/src/mprpcapplication.cc" "/home/hjk/Desktop/mpRPC/build/src/CMakeFiles/mprpc.dir/mprpcapplication.cc.o"
+  "/home/hjk/Desktop/mpRPC/src/mprpcchannel.cc" "/home/hjk/Desktop/mpRPC/build/src/CMakeFiles/mprpc.dir/mprpcchannel.cc.o"
+  "/home/hjk/Desktop/mpRPC/src/mprpcconfig.cc" "/home/hjk/Desktop/mpRPC/build/src/CMakeFiles/mprpc.dir/mprpcconfig.cc.o"
+  "/home/hjk/Desktop/mpRPC/src/mprpccontroller.cc" "/home/hjk/Desktop/mpRPC/build/src/CMakeFiles/mprpc.dir/mprpccontroller.cc.o"
+  "/home/hjk/Desktop/mpRPC/src/rpcheader.pb.cc" "/home/hjk/Desktop/mpRPC/build/src/CMakeFiles/mprpc.dir/rpcheader.pb.cc.o"
+  "/home/hjk/Desktop/mpRPC/src/rpcprovider.cc" "/home/hjk/Desktop/mpRPC/build/src/CMakeFiles/mprpc.dir/rpcprovider.cc.o"
+  "/home/hjk/Desktop/mpRPC/src/zookeeperutil.cc" "/home/hjk/Desktop/mpRPC/build/src/CMakeFiles/mprpc.dir/zookeeperutil.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/include"
+  "../example"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
